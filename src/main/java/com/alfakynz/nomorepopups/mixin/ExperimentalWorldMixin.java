2606779f03cc8479711db5250d9ab1093620e281
Alfakynz <alfakynz.nyxaeris@gmail.com>
2025-09-01 19:30:02 +0200

    Fix experimental world
    - Fix experimental world
    - Update homepage link

---
+++ src/main/java/com/alfakynz/nomorepopups/mixin/ExperimentalWorldMixin.java
@@ -0,0 +1,26 @@
+package com.alfakynz.nomorepopups.mixin;
+
+import com.alfakynz.nomorepopups.config.ModConfig;
+import com.mojang.serialization.Lifecycle;
+import net.minecraft.world.level.LevelProperties;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(LevelProperties.class)
+public class ExperimentalWorldMixin {
+    @Shadow
+    @Final
+    private Lifecycle lifecycle;
+
+    @Inject(method = "getLifecycle()Lcom/mojang/serialization/Lifecycle;", at = @At("HEAD"), cancellable = true)
+    private void getLifecycle(CallbackInfoReturnable<Lifecycle> cir) {
+        if (lifecycle == Lifecycle.experimental() && ModConfig.INSTANCE.disableExperimentalWarning) {
+            cir.setReturnValue(Lifecycle.stable());
+            cir.cancel();
+        }
+    }
+}
