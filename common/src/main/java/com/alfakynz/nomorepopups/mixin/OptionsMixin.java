c5c8ac6e7f5c9d0aa4926b843520627112d34d94
Alfakynz Nyxaeris <141228129+Alfakynz@users.noreply.github.com>
2026-09-23 15:45:55 +0200

    Remove Narrator

--- common/src/main/java/com/alfakynz/nomorepopups/mixin/OptionsMixin.java
+++ common/src/main/java/com/alfakynz/nomorepopups/mixin/OptionsMixin.java
@@ -3,29 +3,34 @@ package com.alfakynz.nomorepopups.mixin;
 import com.alfakynz.nomorepopups.config.ModConfig;
 import net.minecraft.client.Options;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 /**
- * Mixin for removing multiplayer warning.
+ * Mixin for removing the multiplayer warning popup and the narrator onboarding prompt.
  */
 @Mixin(Options.class)
-public class MultiplayerMixin {
+public abstract class OptionsMixin {
+
+    @Shadow public boolean skipMultiplayerWarning;
+    @Shadow public boolean onboardAccessibility;
 
     @Inject(method = "load", at = @At("TAIL"))
-    private void onLoad(CallbackInfo ci) {
-        Options options = (Options) (Object) this;
+    private void nomorepopups$onLoad(CallbackInfo ci) {
         if (ModConfig.general("multiplayer_warning")) {
-            options.skipMultiplayerWarning = true;
+            this.skipMultiplayerWarning = true;
+        }
+        if (ModConfig.general("narrator")) {
+            this.onboardAccessibility = false;
         }
     }
 
     @Inject(method = "save", at = @At("TAIL"))
-    private void onWrite(CallbackInfo ci) {
-        Options options = (Options) (Object) this;
+    private void nomorepopups$onSave(CallbackInfo ci) {
         if (ModConfig.general("multiplayer_warning")) {
-            options.skipMultiplayerWarning = true;
+            this.skipMultiplayerWarning = true;
         }
     }
 }
