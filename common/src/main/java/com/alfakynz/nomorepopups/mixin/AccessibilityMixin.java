056284f0138b72f8e44037d2bc19bf129c983b15
Alfakynz Nyxaeris <141228129+Alfakynz@users.noreply.github.com>
2026-09-24 09:15:38 +0200

    Remove Narrator screen
    - The previous method wasn't the "correct" method to remove the onboarding screen

---
+++ common/src/main/java/com/alfakynz/nomorepopups/mixin/AccessibilityMixin.java
@@ -0,0 +1,28 @@
+package com.alfakynz.nomorepopups.mixin;
+
+import com.alfakynz.nomorepopups.config.ModConfig;
+import net.minecraft.client.gui.screens.AccessibilityOnboardingScreen;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+/**
+ * Mixin for removing the narrator onboarding prompt.
+ */
+@Mixin(AccessibilityOnboardingScreen.class)
+public abstract class AccessibilityMixin {
+
+    @Final
+    @Shadow(remap = false) private Runnable onClose;
+
+    @Inject(method = "init", at = @At("HEAD"), cancellable = true, remap = false)
+    private void nomorepopups$skipOnboarding(CallbackInfo ci) {
+        if (ModConfig.general("narrator")) {
+            ci.cancel();
+            this.onClose.run();
+        }
+    }
+}
